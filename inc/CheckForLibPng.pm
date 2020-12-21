benkasminbullock/image-png-libpng

commit 544f18ce464c5fed832abed09ceef05417a27a3d

Version to 0.52_08
"util/old" script compiles against older versions

Fix problems with inc/CheckForLibPng.pm (separate repository).

Makefile problems for linking against shared object library in
different directory (order of -L in compilation flags and LDDLFLAGS
variable).

Better handling of unused variables and static functions (compiled
with optimization turned on).

--optimize flag for Makefile.PL

--lines flag for build/make-files.pl to switch on/off solaris line numbers

Removed unused @mani from the end of build/make-files.pl

Examples of quantize, graying

Comment out stopping of optimizing

inc/CheckForLibPng.pm
@@ -94,8 +94,19 @@ sub find_program
 
 sub check_for_libpng
 {
-    if ($_[0]) {
+    my (%options) = @_;
+    if ($options{verbose}) {
 	$verbose = 1;
+    }
+    if ($options{prefix}) {
+	$png_include_dir = "$options{prefix}/include";
+	if (! -d $png_include_dir) {
+	    die "$png_include_dir doesn't exist";
+	}
+	$png_lib_dir = "$options{prefix}/lib";
+	if (! -d $png_lib_dir) {
+	    die "$png_lib_dir doesn't exist";
+	}
     }
 
     msg ("Debugging messages in 'check_for_libpng' are switched on");
@@ -116,25 +127,31 @@ sub check_for_libpng
 
     my $libs = '-lpng -lz -lm';
     if ($png_lib_dir) {
-	$libs = "-L $png_lib_dir $libs";
+	$libs = "-L$png_lib_dir $libs";
     }
-    my $has_pkg_config = find_program ('pkg-config', $verbose);
-    if ($has_pkg_config) {
-	msg ('I found "pkg-config" in your PATH so I am going to use that to help with the compilation of the C part of this module.');
-	my $pkg_config_cflags = `pkg-config --cflags libpng`;
-	$pkg_config_cflags =~ s/\s+$//;
-	my $pkg_config_ldflags = `pkg-config --libs libpng`;
-	$pkg_config_ldflags =~ s/\s+$//;
-	if ($pkg_config_cflags) {
-	    msg ("Adding '$pkg_config_cflags' to C compiler flags from pkg-config");
-	    $inc = "$pkg_config_cflags";
-	}
-	if ($pkg_config_ldflags) {
-	    msg ("Adding '$pkg_config_ldflags' to linker flags from pkg-config");
-	    $libs = "$pkg_config_ldflags";
+    if ($options{prefix}) {
+	if ($verbose) {
+	    print "Overriding library and include directories with $options{prefix}.\n";
 	}
     }
-
+    else {
+	my $has_pkg_config = find_program ('pkg-config', $verbose);
+	if ($has_pkg_config) {
+	    msg ('I found "pkg-config" in your PATH so I am going to use that to help with the compilation of the C part of this module.');
+	    my $pkg_config_cflags = `pkg-config --cflags libpng`;
+	    $pkg_config_cflags =~ s/\s+$//;
+	    my $pkg_config_ldflags = `pkg-config --libs libpng`;
+	    $pkg_config_ldflags =~ s/\s+$//;
+	    if ($pkg_config_cflags) {
+		msg ("Adding '$pkg_config_cflags' to C compiler flags from pkg-config");
+		$inc = "$inc $pkg_config_cflags";
+	    }
+	    if ($pkg_config_ldflags) {
+		msg ("Adding '$pkg_config_ldflags' to linker flags from pkg-config");
+		$libs = "$pkg_config_ldflags $libs";
+	    }
+	}
+    }
     # A minimal C program to test compilation and running.
 
     my $test_c = <<'EOF';
@@ -200,7 +217,10 @@ EOF
 	print $output $test_c;
 	close $output
             or die "Error closing file '$c_file_name': $!";
-	my $compile = "$cc $ccflags $inc -o $exe_file_name $c_file_name $ldflags $libs";
+	if ($options{prefix}) {
+	    $libs .= " -Wl,--rpath -Wl,$png_lib_dir ";
+	}
+	my $compile = "$cc $inc $ccflags -o $exe_file_name $c_file_name $libs $ldflags ";
 	msg ("The compile command is '$compile'");
 	$compile_ok = (system ($compile) == 0);
 	if ($compile_ok) {
@@ -285,6 +305,10 @@ EOF
     my %vals;
     $vals{libs} = $libs;
     $vals{inc} = $inc;
+    if ($verbose) {
+	print "libs = $vals{libs}\n";
+	print "inc = $vals{inc}\n";
+    }	
     return \%vals;
 }
 

build/make-files.pl
@@ -15,9 +15,45 @@ BEGIN {
 };
 use autodie;
 use Getopt::Long;
+use File::Compare;
+use File::Copy;
 
-#my $verbose = 1;
-my $verbose;
+my $warned;
+
+my $ok = GetOptions (
+    lines => \my $lines,
+    verbose => \my $verbose,
+);
+
+if (! $ok) {
+    print <<EOF;
+Options:
+
+--lines         - use correct line numbering
+--verbose       - turn on error messages
+
+EOF
+    exit;
+}
+
+if ($verbose) {
+    print "Making the files for Image::PNG::Libpng from templates.\n";
+}
+
+# Copy the file if it is newer.
+
+my $checkerdir = '/home/ben/projects/check4libpng/lib';
+my $checker = 'CheckForLibPng.pm';
+my $checkermaster = "$checkerdir/$checker";
+my $checkercopy = "$Bin/../inc/$checker";
+if (! -f $checkercopy || compare ($checkermaster, $checkercopy)) {
+    if (-f $checkercopy) {
+	chmod 0644, $checkercopy or die $!;
+    }
+    warn "$checker is outdated, overwriting with new version";
+    copy $checkermaster, $checkercopy or die $!;
+    chmod 0444, $checkercopy or die $!;
+}
 
 # Read the configuration file "tmpl/config".
 
@@ -134,13 +170,19 @@ for my $file (@files) {
 	    or die $!;
 	while (<$in>) {
 
-# This is better for my purposes, but it causes errors on
-# SunOS/Solaris compilers:
-# http://www.cpantesters.org/cpan/report/f25ae7b0-94c3-11e3-ae04-8631d666d1b8
-
-#	    s/^(#line)$/sprintf "$1 %d \"tmpl\/%s\"", $. + 1, $template/e; 
-
-	    s/^(#line)$/sprintf "$1 %d \"%s\"", $. + 1, $template/e; 
+	    # This is better for my purposes, but it causes errors on
+	    # SunOS/Solaris compilers:
+	    # http://www.cpantesters.org/cpan/report/f25ae7b0-94c3-11e3-ae04-8631d666d1b8
+	    if ($lines) {
+		if (! $warned) {
+		    warn "Switch off correct line numbers";
+		    $warned = 1;
+		}
+		s/^(#line)$/sprintf "$1 %d \"tmpl\/%s\"", $. + 1, $template/e; 
+	    }
+	    else {
+		s/^(#line)$/sprintf "$1 %d \"%s\"", $. + 1, $template/e; 
+	    }
 	    $text .= $_;
 	}
 	my $outtext;
@@ -167,39 +209,5 @@ for my $file (@files) {
     }
     chmod 0444, $output;
 }
-
-# Other files which aren't made from templates.
-
-my @extras = qw!
-my-xs.c
-tmpl/author
-tmpl/config
-tmpl/examples_doc
-tmpl/generated
-tmpl/libpng_doc
-tmpl/other_modules
-tmpl/png_doc
-tmpl/pngspec
-tmpl/version
-tmpl/warning
-build/ImagePNGBuild.pm
-build/LibpngInfo.pm
-build/make-files.pl
-MANIFEST
-MANIFEST.SKIP
-my-xs.h
-perl-libpng.h
-t/bKGD.t
-t/cHRM.t
-t/pHYs.t
-t/tRNS.t
-t/tIME.t
-README
-!;
-my @mani;
-push @mani, map {"tmpl/$_.tmpl"} @files;
-push @mani, @outputs;
-push @mani, @extras;
-push @mani, 'makeitfile';
 
 exit;

examples/gray2.pl
@@ -1,0 +1,19 @@
+#!/home/ben/software/install/bin/perl
+use warnings;
+use strict;
+use utf8;
+use FindBin '$Bin';
+use Image::PNG::Libpng ':all';
+use Image::PNG::Const ':all';
+
+my $png = create_reader ("/home/ben/life_before_the_pandemic.png");
+$png->read_info ();
+$png->set_rgb_to_gray ();
+print "poo: ", $png->get_rgb_to_gray_status (), "\n";
+$png->read_image ();
+$png->read_end ();
+my $wpng = $png->copy_png ();
+my $ihdr = $wpng->get_IHDR ();
+$ihdr->{color_type}  = PNG_COLOR_TYPE_GRAY_ALPHA;
+$wpng->set_IHDR ($ihdr);
+$wpng->write_png_file ("/home/ben/life-gray.png");

examples/q.pl
@@ -1,0 +1,53 @@
+#!/home/ben/software/install/bin/perl
+use warnings;
+use strict;
+use utf8;
+use FindBin '$Bin';
+use Image::PNG::Libpng ':all';
+my $file = "wave.png";
+my $ncolors = 20;
+my $palette = randompalette ($ncolors);
+my $rpng = create_reader ($file);
+$rpng->set_quantize ($palette, $ncolors, [], 1);
+$rpng->read_png ();
+print scalar (@$palette), "\n";
+my $wpng = copy_png ($rpng);
+$wpng->set_PLTE ($palette);
+$wpng->write_png_file ('wave-quant.png');
+exit;
+
+sub points
+{
+    my ($png, $ncolors) = @_;
+    my $rows = $png->get_rows ();
+    my $h = $png->height ();
+    my $w = $png->width ();
+    my $ch = $png->get_channels ();
+    my @p;
+    for (0..$ncolors-1) {
+	my $x = int (rand ($w));
+	my $y = int (rand ($h));
+	my $row = $rows->[$y];
+	my @pix = split ('', substr ($row, $x*$ch, $ch));
+	push @p, {
+	    red => ord ($pix[0]),
+	    green => ord ($pix[1]),
+	    blue => ord ($pix[2]),
+	};
+    }
+    return \@p;
+}
+
+sub randompalette
+{
+    my ($n) = @_;
+    my @p;
+    for (0..$n-1) {
+	my %color;
+	for my $c (qw!red green blue!) {
+	    $color{$c} = int (rand (256))
+	}
+	push @p, \%color;
+    }
+    return \@p;
+}

examples/rgb-to-gray.pl
@@ -1,0 +1,18 @@
+#!/home/ben/software/install/bin/perl
+use warnings;
+use strict;
+use utf8;
+use FindBin '$Bin';
+use Image::PNG::Libpng ':all';
+use Image::PNG::Const ':all';
+
+my $png = create_reader ("$Bin/loveheartface.png");
+$png->read_info ();
+$png->set_rgb_to_gray ();
+$png->read_image ();
+$png->read_end ();
+my $wpng = $png->copy_png ();
+my $ihdr = $wpng->get_IHDR ();
+$ihdr->{color_type}  = PNG_COLOR_TYPE_GRAY_ALPHA;
+$wpng->set_IHDR ($ihdr);
+$wpng->write_png_file ("$Bin/grayface.png");

t/transformations.t
@@ -17,23 +17,20 @@ BEGIN: {
     use IPNGLT;
 };
 
-my $png = read_png_file ("$Bin/tantei-san.png",
-			 transforms => PNG_TRANSFORM_EXPAND);
-my $ihdr = $png->get_IHDR ();
-is ($ihdr->{color_type}, PNG_COLOR_TYPE_RGB);
-is ($ihdr->{bit_depth}, 8);
-
-my $png2 = read_png_file ("$Bin/tantei-san.png",
-			 transforms => PNG_TRANSFORM_EXPAND |
-			 PNG_TRANSFORM_EXPAND_16);
-my $ihdr2 = $png2->get_IHDR ();
-is ($ihdr2->{color_type}, PNG_COLOR_TYPE_RGB);
-is ($ihdr2->{bit_depth}, 16);
-# my $wpng2 = copy_png ($png2);
-# $wpng2->set_scale_16 ();
-# my $rpng2 = round_trip ($wpng2, "$Bin/tantei-rgb.png");
-# my $rihdr2 = $rpng2->get_IHDR ();
-# is ($rihdr2->{color_type}, PNG_COLOR_TYPE_RGB);
-# is ($rihdr2->{bit_depth}, 8);
+if (libpng_supports ('READ_EXPAND')) {
+    my $png = read_png_file ("$Bin/tantei-san.png",
+			     transforms => PNG_TRANSFORM_EXPAND);
+    my $ihdr = $png->get_IHDR ();
+    is ($ihdr->{color_type}, PNG_COLOR_TYPE_RGB);
+    is ($ihdr->{bit_depth}, 8);
+}
+if (libpng_supports ('READ_EXPAND_16')) {
+    my $png2 = read_png_file ("$Bin/tantei-san.png",
+			      transforms => PNG_TRANSFORM_EXPAND |
+			      PNG_TRANSFORM_EXPAND_16);
+    my $ihdr2 = $png2->get_IHDR ();
+    is ($ihdr2->{color_type}, PNG_COLOR_TYPE_RGB);
+    is ($ihdr2->{bit_depth}, 16);
+}
 
 done_testing ();

tmpl/Libpng.pod.tmpl
@@ -1888,6 +1888,13 @@ allowed. See also L</set_chunk_cache_max>.
 
 [% cor(fn="png_get_chunk_cache_max") %]
 
+=head2 get_rgb_to_gray_status
+
+    my $badpixels = $png->get_rgb_to_gray_status ();
+
+Returns a true value if there were some non-gray pixels in an RGB
+image after calling L</set_rgb_to_gray>.
+
 [% FOR what IN ['height', 'width'] %]
 
 =head2 get_user_[% what %]_max
@@ -1977,6 +1984,40 @@ contain floating-point arguments.  [% manual %]
 [% supports(desc='set_gamma', val='READ_GAMMA') %]
 
 [% cor(fn="set_gamma") %]
+
+=head2 set_quantize
+
+    $png->set_quantize (\@palette);
+
+=head2 set_rgb_to_gray
+
+    $png->set_rgb_to_gray ();
+
+    $png->set_rgb_to_gray ($error_action, $red_weight, $green_weight);
+
+Convert a PNG image from RGB (colored) to gray. [% manual %]
+
+Without arguments, the libpng default values are used for
+C<$red_weight> and C<$green_weight>, and an error action of "none" is
+chosen. If the error action is set to a value of
+C<PNG_ERROR_ACTION_WARN> or C<PNG_ERROR_ACTION_ERROR>, pixels in the
+RGB image where the R, G and B values are not equal will cause a
+warning or error respectively. To get the default values for
+C<$red_weight> or C<$green_weight>, use any negative value. [% manual
+%]
+
+If you choose C<PNG_ERROR_ACTION_NONE> you can find out whether there
+were non-RGB pixels in your image. See L</get_rgb_to_gray_status>.
+
+[% cor(fn="png_set_rgb_to_gray") %] with C<$error_action> having a
+default value of C<PNG_ERROR_ACTION_NONE>, and C<$red_weight> and
+C<$green_weight> both set to negative values.
+
+=head3 Convert a colored image to gray
+
+[% example("rgb-to-gray") %]
+
+[% eximg("grayface") %]
 
 =head2 set_user_limits
 

tmpl/Libpng.xs.tmpl
@@ -26,7 +26,7 @@ PROTOTYPES: ENABLE
 
 INCLUDE: const-xs.inc
 
-# Constructors and destructors
+ # Constructors and destructors
 
 Image::PNG::Libpng
 perl_png_create_read_struct ()
@@ -43,7 +43,7 @@ CODE:
 OUTPUT:
         RETVAL
 
-# XS destructor, this does the work.
+ # XS destructor, this does the work.
 
 void
 perl_png_DESTROY (Png)
@@ -53,7 +53,7 @@ perl_png_DESTROY (Png)
         	perl_png_destroy (Png);
 	}
 
-# No-ops supplied since they used to be in use in old versions of the module.
+ # No-ops supplied since they used to be in use in old versions of the module.
 
 void
 perl_png_destroy_read_struct (Png)
@@ -69,7 +69,7 @@ CODE:
         perl_png_destroy_write_struct (Png);
 
 
-# libpng-style input/output functions
+ # libpng-style input/output functions
 
 void
 perl_png_write_image(Png, rows)
@@ -314,72 +314,7 @@ perl_png_set_keep_unknown_chunks (Png, keep, chunk_list = 0)
 CODE:
         perl_png_set_keep_unknown_chunks (Png, keep, chunk_list);
 
-
-SV *
-perl_png_get_tRNS_palette (Png)
-        Image::PNG::Libpng Png
-CODE:
-        RETVAL = perl_png_get_tRNS_palette (Png);
-OUTPUT:
-        RETVAL
-
-
-void
-perl_png_set_expand (Png)
-        Image::PNG::Libpng Png
-CODE:
-#ifdef PNG_READ_EXPAND_SUPPORTED
-        png_set_expand (Png->png);
-#else
-	UNSUPPORTED("READ_EXPAND");
-#endif /* READ_EXPAND */
-
-
-void
-perl_png_set_expand_16 (Png)
-        Image::PNG::Libpng Png
-CODE:
-#ifdef PNG_READ_EXPAND_16_SUPPORTED
-        png_set_expand_16 (Png->png);
-#else
-	UNSUPPORTED("READ_EXPAND_16");
-#endif /* READ_EXPAND_16 */
-
-
-void
-perl_png_set_filler (Png, filler, flags)
-        Image::PNG::Libpng Png
-        int filler
-        int flags
-CODE:
-	if (Png->type == perl_png_read_obj) { 
-#if defined(PNG_READ_FILLER_SUPPORTED)
-	    png_set_filler (Png->png, filler, flags);
-#else
-	    UNSUPPORTED("READ_FILLER");
-#endif /* read/write filler */
-	}
-	else {
-#if defined(PNG_WRITE_FILLER_SUPPORTED)
-	    png_set_filler (Png->png, filler, flags);
-#else
-	    UNSUPPORTED("WRITE_FILLER");
-#endif /* read/write filler */
-	}
-
-
-void
-perl_png_set_gray_to_rgb (Png)
-        Image::PNG::Libpng Png
-CODE:
-#ifdef PNG_READ_GRAY_TO_RGB_SUPPORTED
-        png_set_gray_to_rgb (Png->png);
-#else
-	UNSUPPORTED("READ_GRAY_TO_RGB");
-#endif /* READ_GRAY_TO_RGB */
-
-
-# Chunk code which is not automatically generated.
+ # Chunk code which is not automatically generated.
 
 int
 perl_png_get_sRGB (Png)
@@ -395,12 +330,22 @@ perl_png_set_sRGB (Png, sRGB)
         Image::PNG::Libpng Png
         int sRGB
 CODE:
-        png_set_sRGB (Png->png, Png->info, sRGB);
+        perl_png_set_sRGB (Png, sRGB);
 
 
-# Transform routines. These generally doesn't take any arguments but
-# usually do require conditional wrappers to compile on older
-# versions of libpng.
+
+SV *
+perl_png_get_tRNS_palette (Png)
+        Image::PNG::Libpng Png
+CODE:
+        RETVAL = perl_png_get_tRNS_palette (Png);
+OUTPUT:
+        RETVAL
+
+
+ # Transform routines. These generally doesn't take any arguments but
+ # usually do require conditional wrappers to compile on older
+ # versions of libpng.
 
 void
 perl_png_set_add_alpha(Png, filler, filler_loc)
@@ -412,14 +357,14 @@ CODE:
 #if defined(PNG_READ_FILLER_SUPPORTED)
 	    png_set_add_alpha (Png->png, filler, filler_loc);
 #else
-	    UNSUPPORTED("READ_FILLER");
+	    UNSUPPORTED(READ_FILLER);
 #endif /* read/write filler */
 	}
 	else {
 #if defined(PNG_WRITE_FILLER_SUPPORTED)
 	    png_set_add_alpha (Png->png, filler, filler_loc);
 #else
-	    UNSUPPORTED("WRITE_FILLER");
+	    UNSUPPORTED(WRITE_FILLER);
 #endif /* read/write filler */
 	}
 
@@ -432,7 +377,7 @@ CODE:
 #ifdef PNG_READ_ALPHA_MODE_SUPPORTED
 	png_set_alpha_mode (Png->png, mode, screen_gamma);
 #else
-	UNSUPPORTED('READ_ALPHA_MODE');
+	UNSUPPORTED(READ_ALPHA_MODE);
 #endif /* def PNG_READ_ALPHA_MODE */
 
 
@@ -444,16 +389,71 @@ CODE:
 #if defined(PNG_READ_BGR_SUPPORTED)
             png_set_bgr (Png->png);
 #else
-	    UNSUPPORTED("READ_BGR");
+	    UNSUPPORTED (READ_BGR);
 #endif /* READ_BGR */
 	}
 	else {
 #if defined(PNG_WRITE_BGR_SUPPORTED)
             png_set_bgr (Png->png);
 #else
-	    UNSUPPORTED("WRITE_BGR");
+	    UNSUPPORTED (WRITE_BGR);
 #endif /* WRITE_BGR */
 	}
+
+
+void
+perl_png_set_expand (Png)
+        Image::PNG::Libpng Png
+CODE:
+#ifdef PNG_READ_EXPAND_SUPPORTED
+        png_set_expand (Png->png);
+#else
+	UNSUPPORTED (READ_EXPAND);
+#endif /* READ_EXPAND */
+
+
+void
+perl_png_set_expand_16 (Png)
+        Image::PNG::Libpng Png
+CODE:
+#ifdef PNG_READ_EXPAND_16_SUPPORTED
+        png_set_expand_16 (Png->png);
+#else
+	UNSUPPORTED (READ_EXPAND_16);
+#endif /* READ_EXPAND_16 */
+
+
+void
+perl_png_set_filler (Png, filler, flags)
+        Image::PNG::Libpng Png
+        int filler
+        int flags
+CODE:
+	if (Png->type == perl_png_read_obj) { 
+#if defined(PNG_READ_FILLER_SUPPORTED)
+	    png_set_filler (Png->png, filler, flags);
+#else
+	    UNSUPPORTED (READ_FILLER);
+#endif /* read/write filler */
+	}
+	else {
+#if defined(PNG_WRITE_FILLER_SUPPORTED)
+	    png_set_filler (Png->png, filler, flags);
+#else
+	    UNSUPPORTED (WRITE_FILLER);
+#endif /* read/write filler */
+	}
+
+
+void
+perl_png_set_gray_to_rgb (Png)
+        Image::PNG::Libpng Png
+CODE:
+#ifdef PNG_READ_GRAY_TO_RGB_SUPPORTED
+        png_set_gray_to_rgb (Png->png);
+#else
+	UNSUPPORTED (READ_GRAY_TO_RGB);
+#endif /* READ_GRAY_TO_RGB */
 
 
 void
@@ -463,7 +463,7 @@ CODE:
 #ifdef PNG_READ_EXPAND_SUPPORTED
 	png_set_expand_gray_1_2_4_to_8 (Png->png);
 #else
-	UNSUPPORTED("READ_EXPAND");
+	UNSUPPORTED (READ_EXPAND);
 #endif /* read_expand */
 
 
@@ -474,7 +474,7 @@ CODE:
 #ifdef PNG_READ_INVERT_ALPHA_SUPPORTED
 	png_set_invert_alpha (Png->png);
 #else
-	UNSUPPORTED("READ_INVERT_ALPHA");
+	UNSUPPORTED (READ_INVERT_ALPHA);
 #endif /* READ_INVERT_ALPHA */
 
 
@@ -485,7 +485,7 @@ CODE:
 #ifdef PNG_READ_INVERT_SUPPORTED
 	png_set_invert_mono (Png->png);
 #else
-	UNSUPPORTED("READ_INVERT");
+	UNSUPPORTED (READ_INVERT);
 #endif /* READ_INVERT */
 
 
@@ -497,14 +497,14 @@ CODE:
 #if defined(PNG_READ_PACK_SUPPORTED)
             png_set_packing (Png->png);
 #else
-	    UNSUPPORTED("READ_PACK");
+	    UNSUPPORTED (READ_PACK);
 #endif /* READ_PACK */
 	}
 	else {
 #if defined(PNG_WRITE_PACK_SUPPORTED)
             png_set_packing (Png->png);
 #else
-	    UNSUPPORTED("WRITE_PACK");
+	    UNSUPPORTED (WRITE_PACK);
 #endif /* WRITE_PACK */
 	}
 
@@ -516,14 +516,14 @@ CODE:
 #if defined(PNG_READ_PACKSWAP_SUPPORTED)
             png_set_packing (Png->png);
 #else
-	    UNSUPPORTED("READ_PACKSWAP");
+	    UNSUPPORTED (READ_PACKSWAP);
 #endif /* READ_PACKSWAP */
 	}
 	else {
 #if defined(PNG_WRITE_PACKSWAP_SUPPORTED)
 	    png_set_packswap (Png->png);
 #else
-	    UNSUPPORTED("WRITE_PACKSWAP");
+	    UNSUPPORTED (WRITE_PACKSWAP);
 #endif /* WRITE_PACKSWAP */
 	}
 
@@ -535,7 +535,7 @@ CODE:
 #ifdef PNG_READ_EXPAND_SUPPORTED
 	png_set_palette_to_rgb (Png->png);
 #else
-	UNSUPPORTED("READ_EXPAND");
+	UNSUPPORTED (READ_EXPAND);
 #endif /* READ_EXPAND */
 
 
@@ -546,7 +546,7 @@ CODE:
 #ifdef PNG_READ_SCALE_16_TO_8_SUPPORTED
 	png_set_scale_16 (Png->png);
 #else
-	UNSUPPORTED("READ_SCALE_16_TO_8");
+	UNSUPPORTED (READ_SCALE_16_TO_8);
 #endif /* READ_SCALE_16_TO_8 */
 
 
@@ -557,7 +557,7 @@ CODE:
 #ifdef PNG_READ_EXPAND_SUPPORTED
 	png_set_tRNS_to_alpha(Png->png);
 #else
-	UNSUPPORTED("READ_EXPAND");
+	UNSUPPORTED (READ_EXPAND);
 #endif /* read_expand */
 
 
@@ -568,7 +568,7 @@ CODE:
 #ifdef PNG_READ_STRIP_16_TO_8_SUPPORTED
         png_set_strip_16 (Png->png);
 #else 
-	UNSUPPORTED("READ_STRIP_16_TO_8");
+	UNSUPPORTED (READ_STRIP_16_TO_8);
 #endif /* READ_STRIP_16_TO_8 */
 
 
@@ -579,7 +579,7 @@ CODE:
 #ifdef PNG_READ_STRIP_ALPHA_SUPPORTED
         png_set_strip_alpha (Png->png);
 #else 
-	UNSUPPORTED("READ_STRIP_ALPHA");
+	UNSUPPORTED (READ_STRIP_ALPHA);
 #endif /* READ_STRIP_ALPHA */
 
 
@@ -591,14 +591,14 @@ CODE:
 #if defined(PNG_READ_SWAP_SUPPORTED)
 	    png_set_swap (Png->png);
 #else
-	    UNSUPPORTED("WRITE_SWAP");
+	    UNSUPPORTED (WRITE_SWAP);
 #endif
 	}
 	else {
 #if defined(PNG_WRITE_SWAP_SUPPORTED)
 	    png_set_swap (Png->png);
 #else
-	    UNSUPPORTED("READ_SWAP");
+	    UNSUPPORTED (READ_SWAP);
 #endif
 	}
 
@@ -609,7 +609,7 @@ CODE:
 #ifdef PNG_READ_SWAP_ALPHA_SUPPORTED
 	png_set_swap_alpha (Png->png);
 #else
-	UNSUPPORTED("READ_SWAP_ALPHA");
+	UNSUPPORTED (READ_SWAP_ALPHA);
 #endif /* READ_SWAP_ALPHA */
 
 
@@ -626,20 +626,21 @@ int
 perl_png_get_palette_max(Png)
 	Image::PNG::Libpng Png;
 CODE:
+	RETVAL = -1;
 #ifdef PNG_CHECK_FOR_INVALID_INDEX_SUPPORTED
 #  ifdef PNG_GET_PALETTE_MAX_SUPPORTED
 	RETVAL = png_get_palette_max (Png->png, Png->info);
 #  else
-	UNSUPPORTED("GET_PALETTE_MAX");
+	UNSUPPORTED (GET_PALETTE_MAX);
 #  endif
 #else
-        UNSUPPORTED("CHECK_FOR_INVALID_INDEX");
+        UNSUPPORTED (CHECK_FOR_INVALID_INDEX);
 #endif
 OUTPUT:
 	RETVAL
 
 
-# Functions which retrieve individual values from the header.
+ # Functions which retrieve individual values from the header.
 
 
 [% FOR what IN ['width', 'height'] %]
@@ -688,11 +689,11 @@ CODE:
 OUTPUT:
 	RETVAL
 
-# http://www.cpantesters.org/cpan/report/fc1cade6-3f17-11eb-9d08-9e4a1f24ea8f
+ # http://www.cpantesters.org/cpan/report/fc1cade6-3f17-11eb-9d08-9e4a1f24ea8f
 
 #ifdef PNG_SET_USER_LIMITS_SUPPORTED
 
-int
+void
 perl_png_set_user_limits (Png, w, h)
 	Image::PNG::Libpng Png;
 	unsigned w;
@@ -712,7 +713,6 @@ OUTPUT:
 [% END # what %]
 
 
-
 void
 perl_png_set_chunk_cache_max(Png, max)
 	Image::PNG::Libpng Png;
@@ -721,8 +721,9 @@ CODE:
 #ifdef PNG_CHUNK_CACHE_MAX_SUPPORTED
 	png_set_chunk_cache_max(Png->png, max);
 #else
-	UNSUPPORTED ('CHUNK_CACHE_MAX');
+	UNSUPPORTED (CHUNK_CACHE_MAX);
 #endif /* CHUNK_CACHE_MAX */
+
 
 int
 perl_png_get_chunk_cache_max(Png)
@@ -732,12 +733,13 @@ CODE:
 	RETVAL = png_get_chunk_cache_max(Png->png);
 #else
 	RETVAL = -1;
-	UNSUPPORTED ('CHUNK_CACHE_MAX');
+	UNSUPPORTED (CHUNK_CACHE_MAX);
 #endif /* CHUNK_CACHE_MAX */
 OUTPUT:
 	RETVAL
 
 #endif /* SET_USER_LIMITS */
+
 
 void
 perl_png_set_image_data (Png, image_data, own = & PL_sv_undef)
@@ -751,6 +753,7 @@ CODE:
 	Png->image_data = image_data;
 	Png->memory_gets++;
 
+
 void
 perl_png_set_row_pointers (Png, row_pointers)
 	Image::PNG::Libpng Png;
@@ -763,12 +766,12 @@ CODE:
         png_set_rows (Png->png, Png->info, Png->row_pointers);
 	Png->memory_gets++;
 
-# These functions predate the macro and were valid in libpng 1.2, yet
-# the macro is invalid except for libpng 1.6.17, so conditional
-# compilation doesn't seem to offer any benefit, hence the following
-# is commented out. 
+ # These functions predate the macro and were valid in libpng 1.2, yet
+ # the macro is invalid except for libpng 1.6.17, so conditional
+ # compilation doesn't seem to offer any benefit, hence the following
+ # is commented out. 
 
-# // #ifdef PNG_WRITE_CUSTOMIZE_COMPRESSION_SUPPORTED
+ # #ifdef PNG_WRITE_CUSTOMIZE_COMPRESSION_SUPPORTED
 
 void
 perl_png_set_compression_level (Png, level)
@@ -828,10 +831,10 @@ perl_png_set_compression_window_bits  (Png,   window_bits);
 CODE:
 	png_set_compression_window_bits (Png->png, window_bits);
 
-# // #endif /* WRITE_CUSTOMIZE_COMPRESSION */
+ # #endif /* WRITE_CUSTOMIZE_COMPRESSION */
 
 
-# This macro is not documented in CHANGES in libpng.
+ # This macro is not documented in CHANGES in libpng.
 
 #ifdef PNG_WRITE_CUSTOMIZE_ZTXT_COMPRESSION_SUPPORTED
 
@@ -870,16 +873,18 @@ CODE:
 
 
 #if 0
+
 void
 perl_png_set_compression_method (Png, method);
 	Image::PNG::Libpng Png;
 	int method;
 CODE:
 	png_set_compression_method (Png->png, method);
+
 #endif /* 0 */
 
-
 #if 0
+
 void
 perl_png_set_crc_action  (Png, crit_action, ancil_action);
 	Image::PNG::Libpng Png;
@@ -887,6 +892,7 @@ perl_png_set_crc_action  (Png, crit_action, ancil_action);
 	int ancil_action;
 CODE:
 	png_set_crc_action (Png->png, crit_action, ancil_action);
+
 #endif /* 0 */
 
 
@@ -899,7 +905,7 @@ CODE:
 #ifdef PNG_READ_GAMMA_SUPPORTED
 	png_set_gamma (Png->png, gamma, override_gamma);
 #else
-	UNSUPPORTED("READ_GAMMA");
+	UNSUPPORTED (READ_GAMMA);
 #endif /* READ_GAMMA */
 	
 
@@ -911,7 +917,7 @@ CODE:
 #ifdef PNG_MNG_FEATURES_SUPPORTED
 	png_permit_mng_features(Png->png, mask);
 #else
-	UNSUPPORTED("MNG_FEATURES");
+	UNSUPPORTED (MNG_FEATURES);
 #endif /* MNG_FEATURES */
 
 
@@ -923,15 +929,38 @@ CODE:
 #ifdef PNG_READ_SHIFT_SUPPORTED
 	png_set_shift (Png->png)
 #else
-	UNSUPPORTED("READ_SHIFT");
+	UNSUPPORTED (READ_SHIFT);
 #endif /* READ_SHIFT */
 [% END %]
 
 
-# Transform functions which require arguments.
+ # Transform functions which require arguments.
+
+void
+perl_png_set_quantize(Png, palette, max_screen_colors, histogram, full_quantize_sv)
+	Image::PNG::Libpng Png;
+	AV * palette;
+	int max_screen_colors;
+	AV * histogram;
+	SV * full_quantize_sv;
+PREINIT:
+	int full_quantize;
+CODE:
+	full_quantize = SvTRUE (full_quantize_sv);
+	perl_png_set_quantize(Png, palette, max_screen_colors, histogram,
+			      full_quantize);
+
+
+ # These macros are for rgb_to_gray
+
+#define DEFAULT_WEIGHT -1.0
+
+#ifndef PNG_ERROR_ACTION_NONE
+#define PNG_ERROR_ACTION_NONE 1
+#endif /* ndef PNG_ERROR_ACTION_NONE */
 	
 void
-perl_png_set_rgb_to_gray (Png, error_action, red, green)
+perl_png_set_rgb_to_gray (Png, error_action = PNG_ERROR_ACTION_NONE, red = DEFAULT_WEIGHT, green = DEFAULT_WEIGHT)
 	Image::PNG::Libpng Png;
 	int error_action;
 	double red;
@@ -942,6 +971,21 @@ CODE:
 #else
 	UNSUPPORTED ("READ_RGB_TO_GRAY");
 #endif /* READ_RGB_TO_GRAY */
+
+#undef DEFAULT_WEIGHT
+
+int
+perl_png_get_rgb_to_gray_status (Png)
+	Image::PNG::Libpng Png;
+CODE: 
+#ifdef PNG_READ_RGB_TO_GRAY_SUPPORTED
+    	RETVAL = (int) png_get_rgb_to_gray_status (Png->png);
+#else
+	UNSUPPORTED ("READ_RGB_TO_GRAY");
+	RETVAL = 0;
+#endif /* READ_RGB_TO_GRAY */
+OUTPUT:
+	RETVAL
 
 
 void
@@ -956,12 +1000,12 @@ CODE:
 	perl_png_set_back (Png, perl_color, gamma_code, SvTRUE(need_expand),
 			   background_gamma);
 #else
-	UNSUPPORTED("READ_BACKGROUND");
+	UNSUPPORTED (READ_BACKGROUND);
 #endif /* READ_BACKGROUND */
 
 
-# These functions are not part of libpng and do not need preprocessor
-# conditional wrappers.
+ # These functions are not part of libpng and do not need preprocessor
+ # conditional wrappers.
 
 int
 perl_png_color_type_channels (color_type)
@@ -1006,14 +1050,6 @@ PPCODE:
 	XPUSHs(sv_2mortal(info_sv));
 
 
-void
-perl_png_set_verbosity (Png, verbosity = 0)
-        Image::PNG::Libpng Png
-        int verbosity; 
-CODE:
-        perl_png_set_verbosity (Png, verbosity);
-        
-
 int
 perl_png_libpng_supports (what)
         const char * what
@@ -1031,6 +1067,14 @@ CODE:
 OUTPUT:
 	RETVAL
 
+
+void
+perl_png_set_verbosity (Png, verbosity = 0)
+        Image::PNG::Libpng Png
+        int verbosity; 
+CODE:
+        perl_png_set_verbosity (Png, verbosity);
+        
 
 SV *
 perl_png_split_alpha (Png)
@@ -1050,8 +1094,8 @@ OUTPUT:
         RETVAL
 
 
-# The following automatically generates the get and set functions for
-# the chunks.
+ # The following automatically generates the get and set functions for
+ # the chunks.
 
 [%- FOR chunk IN chunks %]
 [%- IF chunk.defined('auto_type') %]
@@ -1066,7 +1110,7 @@ CODE:
 [%- IF chunk.defined('pro') %]
 #else /* [% chunk.pro %] */
 	RETVAL = & PL_sv_undef;
-	UNSUPPORTED ('[% chunk.name %]');
+	UNSUPPORTED ([% chunk.name %]);
 #endif /* [% chunk.pro %] */
 [%- END # pro %]
 OUTPUT:
@@ -1091,7 +1135,7 @@ CODE:
         perl_png_set_[% chunk.name %] (Png, [% chunk.name %]);
 [%- IF chunk.defined('pro') %]
 #else /* [% chunk.pro %] */
-	UNSUPPORTED ('[% chunk.name %]');
+	UNSUPPORTED ([% chunk.name %]);
 #endif /* [% chunk.pro %] */
 [%- END # pro %]
 

commit 3d8858fddb6d1630cee76c7057f284aaee021784

Do not contaminate pkg-config flags
Flags from pkg-config were contaminates with manual '-lpng -lz -lm' flags. As a result you passed both of them to a linker

    gcc  -lpng16 -lz -lpng -lz -lm...

This was a regression against
<https://github.com/benkasminbullock/image-png-libpng/pull/30> caused
by 544f18ce464c5fed832abed09ceef05417a27a3d commit.

This patch fixes it.

inc/CheckForLibPng.pm
@@ -143,12 +143,12 @@ sub check_for_libpng
 	    my $pkg_config_ldflags = `pkg-config --libs libpng`;
 	    $pkg_config_ldflags =~ s/\s+$//;
 	    if ($pkg_config_cflags) {
-		msg ("Adding '$pkg_config_cflags' to C compiler flags from pkg-config");
-		$inc = "$inc $pkg_config_cflags";
+		msg ("Replacing C compiler flags with '$pkg_config_cflags' from pkg-config");
+		$inc = "$pkg_config_cflags";
 	    }
 	    if ($pkg_config_ldflags) {
-		msg ("Adding '$pkg_config_ldflags' to linker flags from pkg-config");
-		$libs = "$pkg_config_ldflags $libs";
+		msg ("Replacing linker flags with '$pkg_config_ldflags' from pkg-config");
+		$libs = "$pkg_config_ldflags";
 	    }
 	}
     }